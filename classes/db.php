b6185733465830e83ecf5e7db6d006e4d58ec7d3
isaacmmarvin <isaacmubiru99@gmail.com>
2019-09-17 12:06:31 +0300

    final

--- classes/db.php
+++ classes/db.php
@@ -22,7 +22,7 @@ private static $_instance = null;
 
     try {
 $this->_pdo = new PDO('mysql:host='.$host.';dbname='.$db.'',$username,$password);
-
+        echo 'successfully connected to db';
     }
     catch (PDOException $e)
     {
