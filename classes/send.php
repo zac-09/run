294033ed974834c60c22833b5038ebea54279018
isaacmmarvin <isaacmubiru99@gmail.com>
2019-09-17 12:47:10 +0300

    final

--- classes/send.php
+++ classes/send.php
@@ -1,7 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/core/int.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/classes/db.php";
-echo 'im alright'; 
 
  $db = new DB();
  $db->getData();
@@ -9,8 +8,8 @@ $result = $db->first() ;
 
  $results = json_encode($result);
 
- echo $results;
-  echo  json_encode($db);
+echo $results;
+ echo  json_encode($db);
 
 
 
