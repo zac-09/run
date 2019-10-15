7cf83edbd614c3f06d98e6251ddc251eecfb78d4
zac <isaacmubiru99@gmail.com>
2019-10-15 11:34:40 +0000

    man

--- index.php
+++ index.php
@@ -353,7 +353,7 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1" id="soil">Soil Mositure</div>
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1" id="soil">Temperature</div>
                       <div id="soil-value" class="h5 mb-0 font-weight-bold text-gray-800"></div>
                     </div>
                     <div class="col-auto">
@@ -400,7 +400,7 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Temperature</div>
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Soil Moisture</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800" id ="water-value"> <span>°C</span></div>
                     </div>
                     <div class="col-auto">
@@ -464,7 +464,7 @@
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Soil Moisture</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Temperature</h6>
                   <div class="dropdown no-arrow">
                     <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
