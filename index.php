14cb9fd026bc95870ba50d4c95615a7f6f369290
isaacmmarvin <isaacmubiru99@gmail.com>
2019-12-12 17:46:03 +0300

    tables

--- index.php
+++ index.php
@@ -450,6 +450,10 @@
                       <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Water Level</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800" id="pir-value"> <span></span></div>
                     </div>
+                    <div class="col">
+                          <div class="progress progress-sm mr-2">
+                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
                     <div class="col-auto">
                       <i class="time" name="time" id = "time3"></i>
                     </div>
