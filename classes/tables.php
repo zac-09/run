af333364ea71f864ab5b18e26509bad22b49005b
isaacmmarvin <isaacmubiru99@gmail.com>
2019-12-12 16:22:50 +0300

    tables

--- classes/tables.php
+++ classes/tables.php
@@ -12,7 +12,7 @@ $results = $db->getAll();
 foreach($results as $result=>$value)
 {
     $time = $value->created_at;
-    $value->created_at = Carbon::createFromFormat('Y-m-d',$time)->diffForHumans(); 
+    $value->created_at = Carbon::createFromFormat('Y-m-d H:i:s', $time )->format("H:i A"); 
 $data[] = $value;
 
 }
