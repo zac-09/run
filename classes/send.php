8c1207aa179197d3fe990e4424093abb3d4bfeda
isaacmmarvin <isaacmubiru99@gmail.com>
2019-12-12 17:15:20 +0300

    tables

--- classes/send.php
+++ classes/send.php
@@ -1,12 +1,21 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/core/int.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/classes/db.php";
+use Carbon\Carbon;
 
  $db = new DB();
  $db->getData();
-$result = $db->first() ;
+ foreach($db as $result=>$value)
+{
+    $time = $value->created_at;
+    // $value->created_at = Carbon::createFromFormat('Y-m-d H:i:s', $time )->format("H:i A"); 
+    $value->created_at = Carbon::createFromFormat('Y-m-d H:i:s', $time )->diffForHumans(['parts' => 3, 'join' => true]); 
+$data[] = $value;
 
- $results = json_encode($result);
+}
+// $result = $db->first() ;
+
+ $results = json_encode($data);
 
 echo $results;
  
