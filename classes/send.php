0c4a02526483e29e366756f50a4edf71ad828acf
isaacmmarvin <isaacmubiru99@gmail.com>
2019-12-12 18:08:12 +0300

    tables

--- classes/send.php
+++ classes/send.php
@@ -6,22 +6,22 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/classes/db.php";
 
 use Carbon\Carbon;
 date_default_timezone_set('Africa/Kampala');
-// $db = new DB();
+$db = new DB();
  
-// $results = $db->getAll();
+$results = $db->getAll();
 
-// foreach($results as $result=>$value)
-// {
-//     $time = $value->created_at;
-//     // $value->created_at = Carbon::createFromFormat('Y-m-d H:i:s', $time )->format("H:i A"); 
-//     $value->created_at = Carbon::createFromFormat('Y-m-d H:i:s', $time )->diffForHumans(); 
-// $data[] = $value;
+foreach($results as $result=>$value)
+{
+    $time = $value->created_at;
+    // $value->created_at = Carbon::createFromFormat('Y-m-d H:i:s', $time )->format("H:i A"); 
+    $value->created_at = Carbon::createFromFormat('Y-m-d H:i:s', $time )->diffForHumans(); 
+$data[] = $value;
 
-// }
+}
 
 
-// print json_encode($data[0]);
-printf("Right now is %s", Carbon::now()->toDateTimeString());
+print json_encode($data[0]);
+
 
 
 // $timestamp = $result['created_at'];
