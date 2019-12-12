13afa3990efd239d5244262b9f3f1acc34dc0720
isaacmmarvin <isaacmubiru99@gmail.com>
2019-12-12 12:41:14 +0300

    tables

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,18 +1,49 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#example').DataTable( {
-      "processing": true,
-      "serverSide": true,
-      "ajax": {
-          "url": "http://134.209.193.225/classes/tables.php",
-          "type": "POST"
-      },
-      "columns": [
-          { "data": "DHT" },
-          { "data": "Water" },
-          { "data": "pir" },
-          { "data": "humidity" },
-          { "data": "created_at" },
-      ]
-  } );
-} );
+
+      //   document.getElementById('page-top').addEventListener('mousemove',soilData);
+
+
+      //   function soilData(){
+      //     var xhr = new XMLHttpRequest();
+      //       xhr.open('GET','classes/send.php',true);
+           
+
+      //       xhr.onload = function(){
+      //         if(this.status == 200){
+      //              var output = '';
+      //         var data = JSON.parse(this.responseText);
+                
+      //     output = data;
+
+      //           document.getElementById('soil-value').innerHTML = output.DHT;
+      //           document.getElementById('water-value').innerHTML = output.WATER;
+      //           document.getElementById('humidity-value').innerHTML = output.HUMIDITY;
+      //           document.getElementById('pir-value').innerHTML = output.PIR;
+      //         }
+      //       }
+      // xhr.send();
+      //   }
+
+      $(document).ready(function(){
+        $.ajax({
+          url : "http://134.209.193.225/classes/lineGraph.php",
+          type : "GET",
+          success : function(data){
+            console.log(data);
+      
+            var labels = [];
+            var values = [];
+            
+      
+            for(var i in data) {
+             
+              humudity = data[i].DHT;
+              $('hud').html(humudity);
+             values.push(data[i].DHT);
+            }
+
+
+          }});
+      
+    });
+        
