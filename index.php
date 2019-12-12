6c63255bc39db35da7adf7bb9532e25816148742
isaacmmarvin <isaacmubiru99@gmail.com>
2019-12-12 17:48:53 +0300

    tables

--- index.php
+++ index.php
@@ -447,7 +447,7 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">water level</div>
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">water</div>
                       <div class="row no-gutters align-items-center">
                         <div class="col-auto">
                           <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800" id="pir-value"></div>
